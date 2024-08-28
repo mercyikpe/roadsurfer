e583e13386bfc4bfaab030160bff7a4f4779ff34
mercyikpe <ikpemercy1@gmail.com>
2024-08-28 12:55:02 +0200

    Fix vitest issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig((c) => ({
   base: '/roadsurfer/',
   plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    root: fileURLToPath(new URL('./', import.meta.url))
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
