b0c36b6f2409e8ebb684d59efbc474ed8275330b
mercyikpe <ikpemercy1@gmail.com>
2024-08-28 12:47:46 +0200

    Add base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+export default defineConfig(() => ({
+  base: '/roadsurfer/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}))
